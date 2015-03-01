d73cafd7a562c381bd4db026a7af902104a830cc
Ricardo Lopez <rlopez4@wisc.edu>
2015-03-01 00:41:14 -0600

    change style of brushstroke points

--- app/src/main/java/bitshifting/aircanvas/Graphics/Entities/BrushStroke.java
+++ app/src/main/java/bitshifting/aircanvas/Graphics/Entities/BrushStroke.java
@@ -8,16 +8,16 @@ import java.util.List;
  */
 public class BrushStroke {
     private String OwnerID;
-    private int color;
+    private float[] color;
     private List<Point> points;
 
-    public BrushStroke(String ownerID, int color) {
+    public BrushStroke(String ownerID, float[] color) {
         this.OwnerID = ownerID;
         this.color = color;
         this.points = new ArrayList<Point>();
     }
 
-    public BrushStroke(String ownerID, int color,  List<Point> points) {
+    public BrushStroke(String ownerID, float[] color,  List<Point> points) {
         this.OwnerID = ownerID;
         this.color = color;
         this.points = points;
@@ -27,7 +27,7 @@ public class BrushStroke {
         return OwnerID;
     }
 
-    public int getColor() {
+    public float[] getColor() {
         return color;
     }
 
